0baf6431146ce2d834523325abb30ef4b7ed04c3
Andrew Or <andrewor14@gmail.com>
2018-07-10 15:32:20 -0400

    Fix paths + minor changes to test slurm scripts

--- slurm/run_test_slurm.sh
+++ slurm/run_test_slurm.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 #
-#SBATCH --nodes=4
-#SBATCH --ntasks=4
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=28
-#SBATCH --mem=240G
-#SBATCH --gres=gpu:4
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=1g
+#SBATCH --gres=gpu:1
 #SBATCH --time=48:00:00
 #
-#SBATCH --job-name=test_clurm
+#SBATCH --job-name=test_slurm
 #SBATCH --output=slurm-%x-%j.out
 #
 #SBATCH --mail-type=begin
 #SBATCH --mail-type=end
-#SBATCH --mail-user=haoyuz@princeton.edu
+#SBATCH --mail-user=andrewor@princeton.edu
 
-SLURM_LOG_DIR=/home/haoyuz
-SCRIPT_PATH=/home/haoyuz/jobs/test_slurm.sh
+SLURM_LOG_DIR=/home/andrewor/logs
+SCRIPT_PATH=/home/andrewor/models/slurm/test_slurm.sh
 
 srun --output=$SLURM_LOG_DIR/slurm-%x-%j-%n.out $SCRIPT_PATH
 
--- slurm/test_slurm.sh
+++ slurm/test_slurm.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-module load cudnn/cuda-9.1/7.1.2
-export PYTHONPATH="$PYTHONPATH:/home/haoyuz/tensorflow-models"
-echo $SLURM_JOB_NODELIST
-echo $SLURMD_NODENAME
-date
-sleep 60
-date
-echo "Hello World!"
+echo "Hey guys"
+module load cudnn/cuda-8.0/7.0
+module load anaconda3/5.0.1
+python /home/andrewor/models/samples/core/get_started/premade_estimator.py
+echo "Signing off!"
+
+#export PYTHONPATH="$PYTHONPATH:/home/andrewor/models"
+#echo $SLURM_JOB_NODELIST
+#echo $SLURMD_NODENAME
+#date
+#sleep 60
+#date
 
