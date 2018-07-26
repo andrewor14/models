b646dfe7c8e8f63803eb8e0ebd1419e3733287b5
Andrew Or <andrewor14@gmail.com>
2018-07-26 17:11:21 -0400

    Refactor plot script: specify labels from cmd line

--- plotting/plot.py
+++ plotting/plot.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import argparse
 import matplotlib.pyplot as plt 
 import math
 import numpy as np
@@ -9,54 +10,95 @@ import scipy.stats as stats
 from subprocess import Popen, PIPE
 import sys 
 
+# Make log file name more human-readable
+def format_name(log_file):
+  name = log_file.lstrip("slurm-")
+  name = re.sub("\..*$", "", name)
+  name = re.sub("[-0-9]+$", "", name)
+  return name
 
-# Parse and plot data from each file
-def plot_data(ax, log_files):
-  for log_file in log_files:
-    Popen(['./parse.py', log_file]).communicate()
-    csv_file = re.sub("\..*$", "", log_file) + ".csv"
-    step, time_elapsed, loss, learning_rate, cross_entropy, train_accuracy, global_step_per_sec =\
-      [], [], [], [], [], [], []
-    first_timestamp = None
-    with open(csv_file, "r") as f:
-      lines = f.readlines()[1:] # skip header
-      for line in lines:
-        s = line.split(",")
-        step.append(int(s[0]))
-        this_timestamp = long(s[1])
-        if first_timestamp is None:
-          first_timestamp = this_timestamp
-        time_elapsed.append(this_timestamp - first_timestamp)
-        loss.append(float(s[2]))
-        learning_rate.append(float(s[3]))
-        cross_entropy.append(float(s[4]))
-        train_accuracy.append(float(s[5]))
-        global_step_per_sec.append(float(s[6]))
-    name = log_file.lstrip("slurm-")
-    name = re.sub("\..*$", "", name)
-    name = re.sub("[-0-9]+$", "", name)
-    # Pick and choose what you want to plot!
-    #ax.plot(step, learning_rate, "-x", label="learning_rate (%s)" % name)
-    #ax.plot(step, cross_entropy, "-x", label="cross_entropy (%s)" % name)
-    #ax.plot(step[1:], global_step_per_sec[1:], "-x", label="global_step_per_sec (%s)" % name)
-    ax.plot(time_elapsed, loss, "-x", label="loss (%s)" % name)
-    #ax.plot(time_elapsed, train_accuracy, "-x", label="train accuracy (%s)" % name)
-    os.remove(csv_file)
+# Return the values for a label, printing all known labels if the one requested is unknown
+def get_values(label, data, known_labels):
+  if label not in data:
+    print "Unknown label '%s'. Choose from %s." % (label, known_labels)
+    sys.exit(1)
+  return data[label]
+
+# Parse and plot data from the specified log file
+def plot_data(x_label, y_label, log_file, ax):
+  Popen(['./parse.py', log_file], stdout=PIPE, stderr=PIPE).communicate()
+  csv_file = re.sub("\..*$", "", log_file) + ".csv"
+  labels = []
+  data = {} # label -> list of values
+  first_timestamp = None
+  # Parse CSV file for values
+  with open(csv_file, "r") as f:
+    lines = f.readlines()
+    # Parse labels
+    for label in lines[0].strip().split(","):
+      if label == "timestamp":
+        label = "time_elapsed"
+      data[label] = []
+      labels.append(label)
+    # Parse data
+    for line in lines[1:]:
+      split = line.strip().split(",")
+      if len(split) != len(labels):
+        raise Exception("Number of values (%s) does not match number of columns in header (%s)"\
+          % (len(split), len(labels)))
+      for i, value in enumerate(split):
+        label = labels[i]
+        # Format value according to label
+        if label == "step":
+          value = int(value)
+        elif label == "time_elapsed":
+          value = long(value)
+          if first_timestamp is None:
+            first_timestamp = value
+          value -= first_timestamp
+        else:
+          value = float(value)
+        data[label].append(value)
+  # Plot the requested labels
+  x_data = get_values(x_label, data, labels)
+  y_data = get_values(y_label, data, labels)
+  name = format_name(log_file)
+  if y_label == "global_step_per_sec":
+    x_data = x_data[1:]
+    y_data = y_data[1:]
+  ax.plot(x_data, y_data, "-x", label=name)
+  # Clean up
+  os.remove(csv_file)
 
 def main():
-  args = sys.argv
-  if len(args) <= 2:
-    print "Usage: plot.py [title] [log_file1] [log_file2] ..."
-    sys.exit(1)
-  title = args[1]
-  log_files = args[2:]
-  out_file = "output.png"
+  # Parse arguments, e.g.
+  # ./plot.py --x time_elapsed --y loss --output loss.png
+  #   --title "Resnet50 cifar10, 2 workers (4 GPUs), 1 server"
+  #   --logs slurm-dist_resnet_cifar10-async-1053120-1-1532482561.out,slurm-dist_resnet_cifar10-sync-1053119-1-1532445251.out
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--x", help="x label", default="time_elapsed")
+  parser.add_argument("--y", help="y label", default="train_accuracy")
+  parser.add_argument("--title", help="plot title")
+  parser.add_argument("--logs", help="comma separated path(s) to one or more log files", required=True)
+  parser.add_argument("--output", help="path to output file")
+  args = parser.parse_args()
+  x_label = args.x
+  y_label = args.y
+  title = args.title
+  log_files = args.logs.split(",")
+  out_file = args.output or y_label + ".png"
+
+  # Plot it
   fig = plt.figure()
   ax = fig.add_subplot(1, 1, 1)
-  plot_data(ax, log_files)
-  ax.set_xlabel("time elapsed (s)")
-  ax.set_ylabel("value")
-  legend = ax.legend(loc="upper center")
+  for log_file in log_files:
+    plot_data(x_label, y_label, log_file, ax)
+  ax.set_xlabel(x_label)
+  ax.set_ylabel(y_label)
+  legend_loc = "upper center"
+  if y_label == "train_accuracy":
+    legend_loc = "lower center"
+  legend = ax.legend(loc=legend_loc)
   ax.set_title(title)
   fig.savefig(out_file, bbox_extra_artists=(legend,), bbox_inches='tight')
   print "Wrote to " + out_file
