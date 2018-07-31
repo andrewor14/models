c333dbc45cfb26769ea5310873e33238f21c8d7e
andrewor14 <andrewor14@gmail.com>
2018-07-31 14:16:54 -0400

    Minor changes to run configs

--- slurm/local_resnet_cifar.sh
+++ slurm/local_resnet_cifar.sh
@@ -6,11 +6,15 @@ TIMESTAMP=`date +%s`
 
 CLUSTER="{\"ps\": [\"localhost:2222\"], \"chief\": [\"localhost:2223\"], \"worker\": [\"localhost:2224\", \"localhost:2225\"]}"
 
+export ANDREW_RESNET_SYNC_ENABLED="true"
+export ANDREW_RESNET_SYNC_AGGREGATE_REPLICAS=2
+export ANDREW_RESNET_SYNC_TOTAL_REPLICAS=2
+
 function start_it() {
   ROLE="$1"
   TASK_INDEX="$2"
   GLOBAL_INDEX="$3"
-  LOG_FILE="$LOG_DIR/$EXPR_NAME-$GLOBAL_INDEX-$TIMESTAMP.out"
+  LOG_FILE="$LOG_DIR/$EXPR_NAME-$TIMESTAMP-$GLOBAL_INDEX.out"
   export TF_CONFIG="{\"cluster\": $CLUSTER, \"task\": {\"type\": \"$ROLE\", \"index\": $TASK_INDEX}}"
   echo "Starting $ROLE-$TASK_INDEX, logging to $LOG_FILE"
   ./dist_resnet_cifar10.sh "$TIMESTAMP" > "$LOG_FILE" 2>&1 &
--- slurm/run_dist_resnet_cifar10_sync.sh
+++ slurm/run_dist_resnet_cifar10_sync.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #
-#SBATCH --nodes=4
-#SBATCH --ntasks=4
+#SBATCH --nodes=6
+#SBATCH --ntasks=6
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=28
 #SBATCH --mem=64G
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:4
 #SBATCH --time=48:00:00
 #
 #SBATCH --job-name=dist_resnet_cifar10_sync
@@ -20,8 +20,8 @@ SCRIPT_PATH=/home/andrewor/models/slurm/dist_resnet_cifar10.sh
 TIMESTAMP=`date +%s`
 
 export ANDREW_RESNET_SYNC_ENABLED="true"
-export ANDREW_RESNET_SYNC_AGGREGATE_REPLICAS=2
-export ANDREW_RESNET_SYNC_TOTAL_REPLICAS=2
+export ANDREW_RESNET_SYNC_AGGREGATE_REPLICAS=4
+export ANDREW_RESNET_SYNC_TOTAL_REPLICAS=4
 
 srun --output="$SLURM_LOG_DIR/slurm-%x-%j-%n-$TIMESTAMP.out" "$SCRIPT_PATH" "$TIMESTAMP"
 
