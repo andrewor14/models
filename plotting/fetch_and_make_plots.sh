bec7a946c17dfff4a55f7212224d88c8ec37b0a7
Andrew Or <andrewor14@gmail.com>
2018-09-12 06:26:58 -0400

    Add helper script to fetch logs

---
+++ plotting/fetch_and_make_plots.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+LOG_DIR="/Users/andrew/Documents/dev/tf-logs"
+TAG="$1"
+
+if [[ -z "$TAG" ]]; then
+  echo "No tag provided. Exiting."
+  exit 1
+fi
+
+LOG_DIR="$LOG_DIR/$TAG"
+for dir in `ls "$LOG_DIR"`; do
+  if [[ -d "$LOG_DIR/$dir" ]] && [[ "$dir" == run* ]]; then
+    echo "Processing logs in $LOG_DIR/$dir"
+    cp "$LOG_DIR/$dir"/*-4-*out .
+    cp "$LOG_DIR/$dir"/*-5-*out .
+    ./make_plots.sh
+    mkdir -p "$TAG/$dir"
+    tar -czf "$dir.zip" *out
+    mv *png "$TAG/$dir"
+    mv *zip "$TAG/$dir"
+    rm *out
+  fi
+done
+
