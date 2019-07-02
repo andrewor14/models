3a4947ff931b5d4ad8fe10cf69203e439f7ce884
Andrew Or <andrewor14@gmail.com>
2019-07-02 15:26:43 -0400

    Skip write graph and checkpoint restore

--- slurm/run_distributed.sh
+++ slurm/run_distributed.sh
@@ -7,11 +7,16 @@
 # Set common configs
 source common_configs.sh
 
+# Models flags
 export NUM_PARAMETER_SERVERS="0"
 export NUM_WORKERS="4"
 export NUM_GPUS_PER_WORKER="0"
 export RESNET_SIZE="56"
 export BATCH_SIZE="32"
+
+# Autoscaling flags
+export AUTOSCALING_DISABLE_WRITE_GRAPH="true"
+export AUTOSCALING_DISABLE_CHECKPOINT_RESTORE="true"
 
 ./run_slurm.sh
 
