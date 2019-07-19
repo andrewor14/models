28e19e65d72abb53509394a228a1f458172b53aa
Andrew Or <andrewor14@gmail.com>
2019-07-19 18:23:58 -0400

    Abstract expand communicator functionality in helper script
    The API in mpi_helper.py decouples spawning from expanding.
    This helps with the implementation of autoscaling because
    we may not want to expand as soon as we spawn the process.

---
+++ official/resnet/mpi_helper.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python3
+
+import os
+
+from mpi4py import MPI
+import tensorflow as tf
+import horovod.tensorflow as hvd
+
+from official.resnet.autoscaling_params import *
+
+
+# Environment variables
+PYTHONPATH = "PYTHONPATH"
+OMPI_MCA_orte_output_filename = "OMPI_MCA_orte_output_filename"
+OMPI_MCA_initial_wdir = "OMPI_MCA_initial_wdir"
+MPI_SPAWN_LOG_DIR = "MPI_SPAWN_LOG_DIR"
+MPI_SPAWN_RANK = "MPI_SPAWN_RANK"
+MPI_TAG_NAME = "MPI_TAG_NAME"
+LAUNCH_SCRIPT_NAME = "LAUNCH_SCRIPT_NAME"
+
+# Other constants
+LAUNCH_DIRECTORY = os.environ[OMPI_MCA_initial_wdir]
+REDIRECT_SCRIPT_NAME = "redirect_mpi.sh"
+EXECUTABLE = "bash"
+
+# Tag for merging communicators, only used at the root
+MPI_CURRENT_TAG = 14444
+
+def log_fn(msg):
+  tf.compat.v1.logging.info("[MPI helper]: %s" % msg)
+
+def expand(intracomm, intercomm=None):
+  """
+  Expand an existing intracommunicator by merging an intercommunicator into it.
+  All members of both communicators must participate in this process.
+
+  For example, the intercommunicator may be set to the following:
+    - At the root: the communicator returned by MPI.Comm.Spawn
+    - At the spawned worker: the communicator returned by MPI.Comm.Get_parent
+    - At all other nodes: None
+
+  Return a merged intracommunicator ready to be passed into `hvd.init`.
+  """
+  global MPI_CURRENT_TAG
+  is_joining = intracomm.rank == 0 and AUTOSCALING_MASTER_HOST_PORT in os.environ
+  is_root = intracomm.rank == 0 and not is_joining  
+  tag = MPI_CURRENT_TAG if is_root else None
+
+  if is_joining:
+    log_fn("Joining an existing communicator")
+  else:
+    log_fn("Expanding communicator %s (current size %s)" % (intracomm, intracomm.size))
+
+  # Merge the two intercommunicators into an intracommunicator
+  if intercomm is not None:
+    merged_intracomm = intercomm.Merge(is_joining)
+  else:
+    merged_intracomm = MPI.Intracomm(MPI.COMM_NULL)
+
+  # The root broadcasts its tag in both communicators to make sure everyone has the same tag
+  if intercomm is not None:
+    tag = merged_intracomm.bcast(tag, root=0)
+  tag = intracomm.bcast(tag, root=0)
+
+  # Merge the two intracommunicators into an intercommunicator
+  log_fn("Merging communicators using tag %s" % tag)
+  if is_joining:
+    super_merged_intercomm = MPI.Intracomm.Create_intercomm(intracomm, 0, merged_intracomm, 0, tag)
+  else:
+    super_merged_intercomm = MPI.Intracomm.Create_intercomm(intracomm, 0, merged_intracomm, 1, tag)
+
+  # Finally, convert this intercommunicator into an intracommunicator
+  comm = super_merged_intercomm.Merge(is_joining)
+  log_fn("Our rank in new communicator = %s (size %s)" % (comm.rank, comm.size))
+
+  # Run some collective operations on this communicator
+  test_communication(comm)
+
+  if is_root:
+    MPI_CURRENT_TAG += 1
+  return comm
+
+def spawn(spawned_rank, launch_script=None, args=[], env={}):
+  """
+  Spawn a process using the given MPI communicator.
+
+  This is a helper for spawning a process that will be merged into an existing communicator.
+  The caller must provide `spawned_rank`, which specifies the expected rank of the spawned
+  process in the new merged communicator.
+
+  This method assumes the caller was launched using `mpirun` with either the `--host` or the
+  `--hostfile` option, which controls the node on which the new process will be launched.
+  """
+  # Set environment variables
+  env = env.copy()
+  env[PYTHONPATH] = os.getenv(PYTHONPATH)
+  env[MPI_SPAWN_LOG_DIR] = os.getenv(OMPI_MCA_orte_output_filename)
+  env[MPI_SPAWN_RANK] = spawned_rank
+  # Note: there is a max character limit for the value of MPI.Info!
+  # Here we take care not to exceed it, otherwise we will see MPI_ERR_INFO_VALUE...
+  env = "\n".join(["%s=%s" % (k, v) for k, v in env.items() if v is not None])
+  if len(env) > MPI.MAX_INFO_VAL:
+    raise ValueError("MPI environment string is longer than MPI_MAX_INFO_VAL(%s):\n%s" %\
+      (MPI.MAX_INFO_VAL, env))
+  info = MPI.Info.Create()
+  info.Set("env", env)
+  # Set arguments, assuming the scripts are in the same directory as this file
+  launch_script = launch_script or os.environ[LAUNCH_SCRIPT_NAME]
+  launch_script = os.path.join(LAUNCH_DIRECTORY, launch_script)
+  redirect_script = os.path.join(LAUNCH_DIRECTORY, REDIRECT_SCRIPT_NAME)
+  args = [redirect_script, launch_script] + args
+  return MPI.COMM_SELF.Spawn(EXECUTABLE, args=args, info=info, maxprocs=1)
+
+def test_communication(comm):
+  """
+  Helper method to make sure basic communication works in the given communicator.
+  """
+  log_fn("Testing communication in communicator %s (size %s)" % (comm, comm.size))
+
+  # Try broadcasting a value
+  value = "[root value]" if comm.rank == 0 else None
+  value = comm.bcast(value, root=0)
+  if comm.rank > 0:
+    log_fn("  Received broadcast from root: %s" % value)
+
+  # Try doing an allreduce
+  value = comm.allreduce(comm.rank, op=MPI.SUM)
+  log_fn("  Allreduce result: %s" % value)
+
+  # Try running horovod
+  log_fn("  Starting horovod with communicator of size %s" % comm.size)
+  hvd.init(comm)
+  hvd_rank = hvd.rank()
+  log_fn("  Running horovod allreduce")
+  # TODO: this line hangs after adding the second worker
+  #result = hvd.allreduce(hvd_rank)
+  #log_fn("Horovod allreduce result = %s" % result)
+  log_fn("  Shutting down horovod")
+  hvd.shutdown()
+
--- slurm/mpi_spawn_test.py
+++ slurm/mpi_spawn_test.py
@@ -1,85 +1,52 @@
 import os
 import sys
+import textwrap
+
 from mpi4py import MPI
+import horovod.tensorflow.keras as hvd
 
-from slurm import mpi_spawn_helper
+from official.resnet import mpi_helper
 
-JOINING_ENV_VAR = "JOINING"
-SERVICE_NAME_ITERATION = "spawn_test_iteration"
+
+AUTOSCALING_MASTER_HOST_PORT = "AUTOSCALING_MASTER_HOST_PORT"
 STARTING_TAG = 100
 MAX_WORKERS = int(os.getenv("MAX_WORKERS", 10))
 
 def log(msg):
-  identifier = os.getenv(JOINING_ENV_VAR, "Master")
+  identifier = os.getenv(AUTOSCALING_MASTER_HOST_PORT,\
+    "Master" if MPI.COMM_WORLD.rank == 0 else "Worker")
   identifier = "%s on %s" % (identifier, MPI.Get_processor_name())
   print("%s: %s" % (identifier, msg))
 
 def main():
   try:
-    algorithm(MPI.COMM_WORLD)
+    algorithm(MPI.COMM_WORLD.Dup())
   except Exception as e:
     log("##### ERROR #####")
     raise e
 
 def algorithm(comm):
   """
-  Start the algorithm with a communicator that only includes the master
+  Start the algorithm with a communicator that only includes the root
   then slowly spawn workers one by one and let them join our communicator.
   """
-  is_joining = JOINING_ENV_VAR in os.environ
-  is_master = comm.rank == 0 and not is_joining
-  iteration = 1
+  is_joining = AUTOSCALING_MASTER_HOST_PORT in os.environ
+  is_root = comm.rank == 0 and not is_joining
   while comm.size < MAX_WORKERS:
-    # Master will publish the iteration number
-    if is_master:
-      info = MPI.Info.Create()
-      info.Set("ompi_global_scope", "true")
-      MPI.Publish_name(SERVICE_NAME_ITERATION, str(iteration), info=info)
-
-    # Wait for everyone to get the latest iteration number
-    comm.barrier()
-    iteration = int(MPI.Lookup_name(SERVICE_NAME_ITERATION))
-
-    log("********** Join remote iteration %s **********" % iteration)
-
-    if is_master or is_joining:
-      if is_master:
-        log("Master is spawning worker %s" % iteration)
-        env = { JOINING_ENV_VAR: "Joined_%s" % iteration }
-        spawn_intercomm = mpi_spawn_helper.spawn(MPI.COMM_SELF, iteration, env=env)
-      else:
-        spawn_intercomm = MPI.Comm.Get_parent()
-      merged_intracomm = spawn_intercomm.Merge(is_joining)
-    else:
-      merged_intracomm = MPI.Intracomm(MPI.COMM_NULL)
-
-    # Wait for everyone to get the tag
-    comm.barrier()
-
-    # Create an intercomm from merging comm with merge_intracomm
-    tag = STARTING_TAG + iteration
-    log("Waiting to super merge using tag %s" % tag)
-    if is_joining:
-      super_merged_intercomm = MPI.Intracomm.Create_intercomm(comm, 0, merged_intracomm, 0, tag)
-    else:
-      super_merged_intercomm = MPI.Intracomm.Create_intercomm(comm, 0, merged_intracomm, 1, tag)
-    log("Super merged into intercomm")
-    # Finally, convert this intercomm into an intracomm
-    comm = super_merged_intercomm.Merge(is_joining)
-    log("Our rank in new communicator = %s (size %s)" % (comm.rank, comm.size))
-
-    # Clean up
-    if is_master:
-      MPI.Unpublish_name(SERVICE_NAME_ITERATION, str(iteration))
-
-    # Try broadcasting a value
-    value = "broadcast(%s)" % iteration if is_master else None
-    value = comm.bcast(value, root=0)
-    if not is_master:
-      log("Received broadcast from master: %s" % value)
-
+    log("========== Join remote, current size = %s ==========" % comm.size)
+    spawn_intercomm = None
+    if is_root:
+      log("Master is spawning worker %s" % comm.size)
+      env = { AUTOSCALING_MASTER_HOST_PORT: "Joined worker(%s)" % comm.size }
+      spawn_intercomm = mpi_helper.spawn(comm.size, env=env)
+    elif is_joining:
+      spawn_intercomm = MPI.Comm.Get_parent()
+    comm = mpi_helper.expand(comm, spawn_intercomm)
     is_joining = False
-    iteration += 1
+  log(textwrap.dedent("""
+    ***********************************************************
+      All done, our rank in final communicator = %s (size %s)
+    ***********************************************************""" % (comm.rank, comm.size)))
 
 if __name__ == "__main__":
   main()
