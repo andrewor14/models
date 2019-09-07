ce04daa34f009cf75d0cb42ad67e66c14eeed65e
Andrew Or <andrewor14@gmail.com>
2019-09-07 03:05:53 -0400

    Fix imagenet broken import

--- official/resnet/keras/keras_imagenet_main.py
+++ official/resnet/keras/keras_imagenet_main.py
@@ -18,6 +18,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import os
+
 from absl import app as absl_app
 from absl import flags
 from absl import logging
