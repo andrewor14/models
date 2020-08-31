c07bf3290b2282fe4862ecb424ec389a7c8b4b31
Andrew Or <andrewor14@gmail.com>
2020-08-31 19:58:15 -0400

    Merge BERT experiment scripts, eval every 100 steps

--- deploy/run_transformer_experiment.sh
+++ deploy/run_transformer_experiment.sh
@@ -2,22 +2,27 @@
 
 export MODEL="transformer"
 export NUM_NODES="1"
-export MPI_VERBOSE="false"
-export ENABLE_XLA="false"
+export ENABLE_XLA="true"
 export ENABLE_MONITOR_MEMORY="true"
-export BATCH_SIZE="${BATCH_SIZE:=32768}"
+export MPI_VERBOSE="false"
+export MAX_NUM_GPUS="8"
 
 # Number of examples that can be processed on the GPU at a given time
 # This should be 2048 for 2080 Ti and 4096 for V100
 export VIRTUAL_NODE_SIZE="${VIRTUAL_NODE_SIZE:=4096}"
 
+# Set the batch size such that the number of virtual nodes on the max GPU configuration is 1
+DEFAULT_BATCH_SIZE="$((MAX_NUM_GPUS * VIRTUAL_NODE_SIZE))"
+export BATCH_SIZE="${BATCH_SIZE:=$DEFAULT_BATCH_SIZE}"
+
 if [[ "$EXPERIMENT_MODE" == "try" ]]; then
   num_gpus_list="${NUM_GPUS:-8}"
   export NUM_STEPS="10"
   export SKIP_EVAL="true"
+  export ENABLE_XLA="false"
   export MPI_VERBOSE="true"
 else
-  num_gpus_list="1 2 4 8"
+  num_gpus_list="${NUM_GPUS:-1 2 4 8}"
   export NUM_STEPS="100000"
   export STEPS_BETWEEN_EVALS="$((NUM_STEPS / 10))"
 fi
@@ -26,7 +31,7 @@ for num_gpus in $num_gpus_list; do
   export NUM_GPUS="$num_gpus"
   export CUDA_VISIBLE_DEVICES="$(seq -s ',' 0 "$((num_gpus - 1))")"
   export NUM_VIRTUAL_NODES_PER_DEVICE="$((BATCH_SIZE / NUM_GPUS / VIRTUAL_NODE_SIZE))"
-  export RUN_TAG="${BATCH_SIZE}bs_${NUM_VIRTUAL_NODES_PER_DEVICE}vn"
+  export RUN_TAG="${BATCH_SIZE}bs_${NUM_GPUS}gpu_${NUM_VIRTUAL_NODES_PER_DEVICE}vn"
   echo "Running experiment $RUN_TAG"
   ./run_distributed.sh
 done
